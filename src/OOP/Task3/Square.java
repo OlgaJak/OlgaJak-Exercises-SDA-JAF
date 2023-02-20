56e4c3f8c6d92dfd89c3ea515d39687ab23c814f
OlgaJak <jakimenko83@mail.ru>
2023-02-20 13:42:28 +0200

    added Task2 Task2 done hopefully comments deleted comment deleted added Task3 added some classes, awaiting Square fulfillment

---
+++ src/OOP/Task3/Square.java
@@ -0,0 +1,19 @@
+package OOP.Task3;
+
+public class Square extends Rectangle {
+    public Square(String colour, boolean isFilled, double width, double length) {
+        super(colour, isFilled, width, length);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setLength(length);
+    }
+
+}
