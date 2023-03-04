22152d2e2c4f4256f207d75e76ad4b7b08e014e7
OlgaJak <jakimenko83@mail.ru>
2023-03-04 14:49:23 +0200

    added solution to Task 3 to Classes and Interfaces exercise

---
+++ src/Interfaces/Task3/Task3.java
@@ -0,0 +1,75 @@
+package Interfaces.Task3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Car car = new Car("Mercedes", "sport");
+        System.out.println(car);
+    }
+}
+
+    class Car {
+        private String name;
+        private String type;
+        private Engine engine;
+
+
+        public Car(String name, String type) {
+            this.name = name;
+            this.type = type;
+            engine = new Car.Engine();
+            engine.setEngineType(type);
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public Engine getEngine() {
+            return engine;
+        }
+
+        public void setEngine(Engine engine) {
+            this.engine = engine;
+        }
+
+        @Override
+        public String toString() {
+            return "Car name: " + name + " ,car type: " + type + " ,engine: " + engine;
+        }
+
+        class Engine {
+            private String engineType;
+
+            public void setEngineType(String carType) {
+                switch (carType) {
+                    case "economy":
+                        engineType = "diesel";
+                        break;
+
+                    case "luxury":
+                        engineType = "electric";
+                        break;
+                    default:
+                        engineType = "petrol";
+                }
+            }
+            @Override
+            public String toString(){
+                return "Engine type is: " + engineType;
+            }
+        }
+    }
+
